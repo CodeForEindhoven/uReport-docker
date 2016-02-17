5ebc94f5ac72d1087380f2b5bb3ac5f9f52ae133
milovanderlinden <milo@dogodigi.net>
2016-02-17 13:57:28 +0100

    Removed dependencies that are now covered by composer

--- run.sh
+++ run.sh
@@ -3,8 +3,8 @@ if [ "$ALLOW_OVERRIDE" = "**False**" ]; then
     unset ALLOW_OVERRIDE
 else
     sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
-    a2enmod rewrite
 fi
+a2enmod rewrite
 
 sed -i "s^__BASE_URI__^$BASE_URI^g" /etc/apache2/sites-available/default
 
